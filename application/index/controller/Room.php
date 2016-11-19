29a62ac3446dcbcffc2af9d17b4e3188ac881c9e
Xiaohaozi <470401911@qq.com>
2016-11-19 15:55:47 +0800

    添加房间的创建添加,以及成员加入退出

--- application/index/controller/Room.php
+++ application/index/controller/Room.php
@@ -7,9 +7,11 @@
  *********************************
  */
 namespace app\index\controller;
+use app\index\Model\PlaylistModel;
 use app\index\model\RoomModel;
 use app\index\model\RoomMemberModel;
 use think\Db;
+use think\Exception;
 
 class Room extends Base{
     public function _initialize(){}
@@ -32,43 +34,101 @@ class Room extends Base{
         Db::startTrans();
         try{
             $time = date('Y-m-d H:i:s');
-            $id = Db::table('ktv_room')->insertGetId(['name'=>$name, 'passwd'=>$passwd, 'creater'=>$uid, 'create_time'=>$time]);
+            $id = Db::table('ktv_room')->insertGetId(['name'=>$name, 'passwd'=>$passwd, 'creater'=>$uid, 'status'=>1,'create_time'=>$time]);
             Db::table('ktv_room_member')->insert(['r_id'=>$id, 'u_id'=>$uid, 'level'=>0, 'join_time'=>$time]);
             Db::commit();
         } catch (\Exception $e){
             Db::rollback();
             return $e;
         }
-        return array('status'=>200, 'message'=>'ok');
+        return $this->_return(200,'ok');
     }
 
     /**
      * 查看附近的房间
-     * @return array
+     * @return \think\response\Json
      */
     public function show(){
         $room = new RoomModel;
         $list = $room->where('status', '0')->select();
-        return array('status'=>200, 'message'=>'ok', 'data'=>$list);
+        return $this->_return(200,'ok',$list);
     }
 
     /**
      * 关闭房间
      * @param string $roomid
      * @param string $uid
+     * @return \think\response\Json
      */
     public function close($roomid = '', $uid = ''){
         //查看用户权限是否足够
-        //关闭房间
-    }
-    
-    public function addMember($uid='', $roomid =''){
-        return array('status'=>200,'ok', 'data'=>$data);
+        $members = new RoomMemberModel;
+        $rooms    = new RoomModel;
+        $member  = $members->get(array('u_id'=>$uid,'r_id'=>$roomid));
+        //不是创建者, 无权关闭该房间
+        if($member->level != 0){
+            return $this->_return(404,'权限不足');
+        }else{
+            //找到对应的房间,删除
+            $room = $rooms->getByr_id($roomid);
+            $room->delete();
+            return $this->_return(200,'ok');
+        }
     }
 
-    public function deleteMember(){
-        
-        return array('status'=>200, 'message'=>'ok', 'data'=>'');
+    /**
+     * 成员加入房间
+     * @param string $roomid
+     * @param string $uid
+     * @param string $passwd
+     * @return \think\response\Json
+     */
+    public function addMember($roomid ='',$uid = '', $passwd = ''){
+        //先查看房间状态, 是否开启
+        $room = RoomModel::get($roomid);
+        //如果房间开启并且密码相同
+        if(isset($room) && ($room->status == 1)) {
+            if($passwd != $roomid){
+                $this->_return(403,'密码错误');
+            }
+            //将用户存入数据库
+            try {
+                $member = new RoomMemberModel;
+                $member->r_id = $roomid;
+                $member->u_id = $uid;
+                $member->join_time = date('Y-m-d H:i:s');
+                $member->save();
+                return $this->_return(200,'ok');
+            }catch (\Exception $e){ //获取异常
+                return $this->_return(403,'请勿重复加入房间');
+            }
+        }else{
+            //房间不存在,加入失败
+            return $this->_return(404,'您所加入的房间不存在');
+        }
+    }
+
+    /**
+     * 用户退出房间
+     * @param string $roomid
+     * @param string $uid
+     * @return \think\response\Json
+     */
+    public function deleteMember($roomid = '', $uid = ''){
+        //查找房间
+        $room = RoomModel::get($roomid);
+        //查找该成员
+        $member = RoomMemberModel::get(array('r_id'=>$roomid, 'u_id'=>$uid));
+        if(!isset($room) || !isset($member)){
+            return $this->_return(404,'房间不存在或不在房间内');
+        }
+        try {
+            //退出
+            $member->delete();
+        }catch (\Exception $e){ //异常
+            return $this->_return(403,'退出房间错误');
+        }
+        return $this->_return(200,'ok');
     }
 
 }
